32eba5f36b791970a0a5d5e559a0eda9ea291973
Kris Hopper <hopperdavis@gmail.com>
2021-06-02 13:45:08 -0700

    add new project for variable scope

---
+++ jQuery/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("div#click-one").click(function() {
+    const whatToSay = "Hello!":
+    alert(whatToSay);
+  })
+
+  $("div#click-one").click(function() {
+    alert(whatToSay);
+  })
+})
