1bce1d6c1b6a646282ff030321e2de0e6ac26c12
Kris Hopper <hopperdavis@gmail.com>
2021-06-02 19:28:52 -0700

    use files to test bugs and add files for calculator practice

--- jQuery/js/scripts.js
+++ jQuery/js/scripts.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
 
   $("div#click-one").click(function() {
-    const whatToSay = "Hello!":
+    const whatToSay = "Hello!";
     alert(whatToSay);
-  })
+  });
 
-  $("div#click-one").click(function() {
+  $("div#click-two").click(function() {
     alert(whatToSay);
-  })
-})
+  });
+
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput = $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+  });
+
+});
