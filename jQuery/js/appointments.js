1f74baca0188900f17b2d1b29aff46f15c39f78c
Kris Hopper <hopperdavis@gmail.com>
2021-06-03 07:31:23 -0700

    update appointments practice, add drinks and car-insurance branching practice

--- jQuery/js/appointments.js
+++ jQuery/js/appointments.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $("form#book-appointment").submit(function (event) {
     event.preventDefault();
-    let name = ("#name");
-    let type = ("#type");
-    let date = ("#date");
-    let time = ("#time");
+    let name = $("#name").val();
+    console.log("Is this working");
+    let type = $("#type").val();
+    let date = $("#date").val();
+    let time = $("#time").val();
     const result = ("Success!");
     $("#output").text(result);
   });
